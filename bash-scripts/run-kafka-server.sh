62f1ff59ef6dfdd77f522f513362f485204289ff
duongnln96 <nguyenlengocduong@gmail.com>
2021-04-25 14:01:00 +0700

    Update how to benchmark kafka

--- bash-scripts/run-kafka-server.sh
+++ bash-scripts/run-kafka-server.sh
@@ -12,7 +12,7 @@ export KAFKA_OPTS="-javaagent:$KAFKA_DIR/prometheus/jmx_prometheus_javaagent-0.6
 export JMX_PORT=7081
 
 if [[ ${opt} == "start" ]]; then
-    $KAFKA_DIR/bin/kafka-server-$opt.sh $KAFKA_DIR/config/server.properties
+    $KAFKA_DIR/bin/kafka-server-$opt.sh ./server-config.properties
 elif [[ ${opt} == "stop" ]]; then
     $KAFKA_DIR/bin/kafka-server-$opt.sh
 fi
